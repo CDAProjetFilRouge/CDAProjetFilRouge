b467edf484b6f2d7159b693edbe84c6895eb3f38
QuentinHamonic <56317359+QuentinHamonic@users.noreply.github.com>
2026-09-11 11:08:48 +0200

    feat: coquille LegalDocumentController

---
+++ src/main/java/fr/diginamic/hubevenementiel/controllers/LegalDocumentController.java
@@ -0,0 +1,10 @@
+package fr.diginamic.hubevenementiel.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/legal-documents")
+public class LegalDocumentController {
+
+}
